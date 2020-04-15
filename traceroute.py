bd3af5ded3cf2c09b7277634a025e494cfaba73d
Sudhir Yadav <sudhir22yadav@gmail.com>
2020-04-15 07:55:12 -0600

    receive function updated

--- traceroute.py
+++ traceroute.py
@@ -19,41 +19,25 @@ IP_PROTO_UDP = 17
 UDP_SRC_PORT = 44354
 UDP_DEST_PORT = random.randint(33434, 33534)
 UDP_LEN = 8
-
-# ICMP Global Variables
-ICMP_TTL_EXCEEDED = 11
-ICMP_DST_UNREACH = 3
-ICMP_TIMEOUT = 1
 PACKET_COUNT = 3
+SECONDS = random.uniform(0.50, 1.0)
 
 
-def receive():
+def receive(ip_dest):
     sck = socket.socket(socket.AF_INET, socket.SOCK_RAW, socket.IPPROTO_ICMP)
     rec_packet, addr = sck.recvfrom(1024)
-    recv_time = time.time()
-    ip_header = rec_packet[:20]
-    ip_version, ip_tos, ip_length, \
-        ip_id, ip_flags, ip_ttl, ip_protocol, \
-        ip_checksum, ip_src, ip_dest = struct.unpack(
-            "!BBHHHBBHII", ip_header)
-    header = rec_packet[20:28]
-    icmp_type, icmp_code, icmp_checksum, \
-        icmp_pid, icmp_seq = struct.unpack("!BBHHH", header)
-    # delay = round((recv_time - start_time) * 1000)
-    ip_src = (socket.inet_ntoa(struct.pack('!L', ip_src)))
-    if icmp_type == ICMP_TTL_EXCEEDED:
+    if addr[0] == ip_dest:
         sck.close()
-        return ip_src, False
-    elif icmp_type == ICMP_DST_UNREACH:
+        return addr[0], True
+    else:
         sck.close()
-        return ip_src, True
+        return addr[0], False
 
 
 def gen_header(ip_dest, ip_ttl, mysocket):
     # IP Header
     ip_check = 0
-    ip_src = '192.168.1.67'
-    
+    ip_src = '0.0.0.0'
     ip_src = socket.inet_aton(ip_src)
     ip_dest = socket.inet_aton(ip_dest)
     ip_header = struct.pack('!BBHHHBBH', IP_VER_IHL, IP_TOS, IP_TOTAL_LEN,
@@ -76,21 +60,19 @@ def traceroute(ip_dest):
     ip_ttl = 1
     hop_ip = ''
     while hop_ip != ip_dest :
-        time.sleep(1)
         delays = []
         for i in range(PACKET_COUNT):
             start_time = time.time()
             header = gen_header(ip_dest, ip_ttl, mysocket)
             mysocket.sendto(header, (ip_dest, 0))
-            hop_ip, check = receive()
+            hop_ip, check = receive(ip_dest)
             recv_time = time.time()
             delays.append( str(round((recv_time - start_time)*1000)) + ' ms')
-            
-            #time.sleep(1)
-        print(ip_ttl, hop_ip, delays[0], delays[1], delays[2])
+            time.sleep(SECONDS)
+        print(ip_ttl, '.', hop_ip, delays[0], delays[1], delays[2])
         ip_ttl += 1
 
-        if check:
+        if check and ip_dest == hop_ip:
             print('Traceroute finished after', ip_ttl, ' Hops')
             mysocket.close()
 
