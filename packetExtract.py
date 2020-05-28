a4172e019e0eb53e3e0aafedd96440e22332c701
Sudhir Yadav <sudhir22yadav@gmail.com>
2020-05-27 20:31:15 -0600

    dhcp and dhcpv6 extraction added

--- packetExtract.py
+++ packetExtract.py
@@ -64,10 +64,12 @@ def eth_frame(data):
     Ethernet Frame, Please pass full data receieved by recvfrom function in sockets.
     It will pass the data minus the ethernet header, which can be used for further extractions
     '''
-    mac_header = data[:14]
-    dst_mac, src_mac, eth_proto = unpack('!6s6sH', mac_header)
+    mac_header = data[:14]  # MAC header is of 14 bytes 
+    dst_mac, src_mac, eth_proto = unpack('!6s6sH', mac_header)  # extracting data using unpack() from struct
+    # converting mac addresses in hex
     dst_mac = hexlify(dst_mac)
     src_mac = hexlify(src_mac)
+    # checking for ip protocol used in the frame
     if eth_proto == 2048:
         proto = 'IPv4'
     elif eth_proto == 2054:
@@ -76,6 +78,7 @@ def eth_frame(data):
         proto = 'IPv6'
     else:
         proto = 'UNKNOWN FOR NOW'
+    # returning the values and data after removing the ethernet header
     return dst_mac, src_mac, proto, data[14:]
 
 
@@ -86,14 +89,18 @@ def arp(data):
     if you are receiving data from from eth_frame(), you can use the same data. if not, then please pass data from 14th byte
     as we are assuming that ethernet header is no more then 14 bytes
     '''
-    arp_packet = data[:28]
-    hw_type, proto_type, hw_addr_len, proto_len_addr, opcode, sender_mac, sender_ip, target_mac, target_ip = unpack(
-        '!HHBBH6sI6sI', arp_packet)
+    arp_packet = data[:28]  # ARP header is of 28 bytes after the ethernet header
+    hw_type, proto_type, hw_addr_len, proto_len_addr, opcode, sender_mac, \
+        sender_ip, target_mac, target_ip = unpack('!HHBBH6sI6sI', arp_packet) 
+    # converting ipv4 addresses into ip readable form
     sender_ip = (inet_ntoa(pack('!L', sender_ip)))
     target_ip = (inet_ntoa(pack('!L', target_ip)))
+    # converting mac addresses in hex
     sender_mac = hexlify(sender_mac)
     target_mac = hexlify(target_mac)
-    return hw_type, proto_type, hw_addr_len, proto_len_addr, opcode, sender_mac, sender_ip, target_mac, target_ip, data[28:]
+    # returning the values and data with arp header removed
+    return hw_type, proto_type, hw_addr_len, proto_len_addr, opcode, sender_mac, \
+        sender_ip, target_mac, target_ip, data[28:]
 
 
 # extracting ipv6 data
@@ -103,12 +110,15 @@ def ipv6(packet):
     if you are receiving data from from eth_frame(), you can use the same data. if not, then please pass data from 14th byte
     as we are assuming that ethernet header is no more then 14 bytes
     '''
-    ipv6_header = packet[:40]
+    ipv6_header = packet[:40]   # Considering this as fixed ipv6 header of 40 bytes
     ver_class_label, payload_length, next_header, hop_limit, src_addr, dst_addr = unpack(
         '!IHBB16s16s', ipv6_header)
+    # converting ipv6 addresses in hex
     src_addr = hexlify(src_addr)
     dst_addr = hexlify(dst_addr)
+    # using bit operation to find the version as first three bits are version field
     version = ver_class_label >> 28
+    # returning values with data with ipv6 header removed
     return version, payload_length, next_header, hop_limit, src_addr, dst_addr, packet[40:]
 
 
@@ -119,15 +129,18 @@ def ipv4(packet):
     if you are receiving data from from eth_frame(), you can use the same data. if not, then please pass data from 14th byte
     as we are assuming that ethernet header is no more then 14 bytes
     '''
-    ip_header = packet[:20]
+    ip_header = packet[:20] # we need first 40 bytes of ipv4 header
     ip_ver_len, ip_tos, ip_length, \
         ip_id, ip_flags, ip_ttl, ip_protocol, \
         ip_checksum, ip_src, ip_dst = unpack(
             "!BBHHHBBHII", ip_header)
+    # using bit operations to find ip_version and ip header length
     ip_version = ip_ver_len >> 4
     ip_header_len = (ip_ver_len & 15) * 4
+    # converting ipv4 addresses into ip readable form
     ip_src = (inet_ntoa(pack('!L', ip_src)))
     ip_dst = (inet_ntoa(pack('!L', ip_dst)))
+    # returning values with data excluding full ipv4 header 
     return ip_version, ip_header_len, ip_ttl, ip_src, ip_dst, ip_protocol, packet[ip_header_len:]
 
 
@@ -160,9 +173,10 @@ def tcp(data):
     TCP header and data extraction, pass data directly returned from ipv4() or ipv6 method, if you are not using any of this.
     please pass data from 34th Byte for IPv4 and 54th Byte for IPv6(keeping in mind that IPv6 has a fixed header) 
     '''
-    tcp_header = data[:14]
+    tcp_header = data[:14]  # we need first 14 bytes of tcp header
     tcp_src_port, tcp_dst_port, seq, ack, off_flags = unpack(
         '!HHLLH', tcp_header)
+    # using bit operations to find the below values
     offset = off_flags >> 12
     flag_urg = (off_flags >> 5) & 1
     flag_ack = (off_flags >> 4) & 1
@@ -170,6 +184,7 @@ def tcp(data):
     flag_rst = (off_flags >> 2) & 1
     flag_syn = (off_flags >> 1) & 1
     flag_fin = off_flags & 1
+    # returning the values and data excluding entire tcp header
     return tcp_src_port, tcp_dst_port, seq, ack, flag_urg, flag_ack, flag_psh, flag_rst, flag_syn, flag_fin, data[offset:]
 
 
@@ -208,3 +223,28 @@ def igmp(data):
     igmp_type, igmp_res_time, igmp_check, group_addr = unpack('!BBHL',igmp_header)
     group_addr = (inet_ntoa(pack('!L', group_addr)))
     return igmp_type, igmp_res_time, igmp_check, group_addr, data[8:]
+
+
+#extracting DHCP 
+def dhcp(data):
+    '''
+    DHCP header and data extraction, data returned from udp() or tcp() will be passed in this. 
+    '''
+    dhcp_header = data[:34]
+    dhcp_opcode, hw_type, hw_addr_len, hop_count, transaction_id, num_seconds, flags, \
+        client_ip_addr, your_ip_addr, srv_ip_addr, gw_ip_addr, client_hw_addr = unpack('!BBBBLHHLLLL6s', dhcp_header)
+    # converting mac addresses in hex
+    client_hw_addr = hexlify(client_hw_addr)
+    return dhcp_opcode, hw_type, hw_addr_len, hop_count, transaction_id, num_seconds, flags, \
+        client_ip_addr, your_ip_addr, srv_ip_addr, gw_ip_addr, client_hw_addr, data[28:]
+
+
+#exctracting DHCPv6
+def dhcpv6(data):
+    '''
+    DHCPv6 header and data extraction, pass data from udp() or tcp() will be passed in this
+    '''
+    message = data[:1]
+    message = unpack('!B', message)
+    return message, data[1:]
+
